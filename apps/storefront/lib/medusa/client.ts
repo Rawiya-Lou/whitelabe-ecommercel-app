5ec37dad32ab3400d749f32ff6c644ff360ead98
Rawiya-lou <raoiua15@gmail.com>
2026-09-11 11:34:23 +0100

    feat(storefront): adding the medusaCLient for the api communications

---
+++ apps/storefront/lib/medusa/client.ts
@@ -0,0 +1,8 @@
+import Medusa from "@medusajs/js-sdk";
+import { env } from "@/app/env.mjs";
+
+export const medusaCLient = new Medusa({
+  baseUrl: env.NEXT_PUBLIC_MEDUSA_BACKEND_URL,
+  debug: process.env.NODE_ENV === "development",
+  publishableKey: env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
+});
