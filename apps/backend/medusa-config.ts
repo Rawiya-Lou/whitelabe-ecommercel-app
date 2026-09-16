4d78bf776cf311146d42fb0b566e285723afc98c
Rawiya-lou <raoiua15@gmail.com>
2026-09-16 12:39:47 +0100

    feat(backend): stripe setup and stripe Cli for testing

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -25,7 +25,22 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET,
     },
   },
-   modules: [
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/payment-stripe",
+            id: "stripe",
+            options: {
+              apiKey: process.env.STRIPE_API_KEY,
+              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: "./src/modules/algerian-logistics",
     },
