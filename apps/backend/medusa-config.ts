98c0e3a8dd2fdb35834730faea1de98f8e13359e
Rawiya-lou <raoiua15@gmail.com>
2026-09-16 13:57:27 +0100

    feat(backend): adding the capture in medusa config file for multi currency

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -36,6 +36,7 @@ module.exports = defineConfig({
             options: {
               apiKey: process.env.STRIPE_API_KEY,
               webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+              capture: true,
             },
           },
         ],
